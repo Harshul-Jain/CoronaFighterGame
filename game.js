e61419b435f90dbbcd76424319ec7eeb7a6303e4
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-05-13 14:42:59 +0530

    Built Viruses

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 function load_image(){
     //players,gems,virus
+    virus_img=new Image();
+    virus_img.src="Assets/v1.png";
 }
 function init(){
     // define all objects that we will have in the game
@@ -10,27 +12,47 @@ function init(){
     canvas.height=H;
     //get Context
     pen=canvas.getContext('2d');
-    box={
+    v1={
         x:150,
         y:50,
         w:60,
         h:60,
-        speed:10,
+        speed:15,
     };
+    v2={
+        x:300,
+        y:150,
+        w:60,
+        h:60,
+        speed:30,
+    };
+    v3={
+        x:450,
+        y:20,
+        w:60,
+        h:60,
+        speed:40,
+    };
+    enemy=[v1,v2,v3];
 }
 function draw(){
     //clear the canvas area for old frame
     pen.clearRect(0,0,W,H);
     pen.fillStyle="red";
-    pen.fillRect(box.x,box.y,box.w,box.h);
+    for(var i=0;i<enemy.length;i++){
+        pen.drawImage(virus_img,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
+    }
     
 }
 function update(){
-    //move the box downwards
-    box.y+=box.speed;
-    if(box.y>=H-box.h || box.y<0){
-        box.speed*=(-1);
+    //move the virus
+    for(var i=0;i<enemy.length;i++){
+        enemy[i].y+=enemy[i].speed;
+        if(enemy[i].y>=H-enemy[i].h || enemy[i].y<=0){
+            enemy[i].speed*=-1;
+        }
     }
+    
     
 }
 function gameloop(){
